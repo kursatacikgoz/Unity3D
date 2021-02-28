d6997c6bc1bf3b3485bb1558ff2b68b1863a60e0
Kürşat Açıkgöz <kursatacikgoz@marun.edu.tr>
2021-02-28 14:53:05 +0300

    Upload

---
+++ Separate Colors/Assets/Scripts/GameController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int countOfSphereNeed = 3;
+    public int sizeOfGlass = 3;
+    public GameObject[] Fields;
+    public GameObject[] ColorPrefabs;
+    public GameObject[] Glasses;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initializeFieldsWithSphere();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void initializeFieldsWithSphere()
+    {
+        int sphereCount = (Fields.Length - countOfSphereNeed) / countOfSphereNeed;
+        int numOfColorPrefabs = ColorPrefabs.Length;
+
+        GameObject[] colors = new GameObject[Fields.Length - countOfSphereNeed];
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            int num = i % (ColorPrefabs.Length);
+            colors[i] = ColorPrefabs[num];
+        }
+
+        reshuffle(colors);
+
+
+
+        for (int i = 0; i < Fields.Length / countOfSphereNeed; i++)
+        {
+            for (int j = 0; j < countOfSphereNeed; j++)
+            {
+                int num = i * countOfSphereNeed + j;
+                GameObject temp;
+                if (num < colors.Length)
+                {
+                    temp = Instantiate(colors[num], Fields[num].transform.position, Quaternion.identity);
+                    temp.GetComponent<Sphere>().gameControl = transform.gameObject;
+                    temp.transform.parent = Fields[num].transform;
+                }
+                else
+                {
+                    temp = null;
+                }
+            }
+        }
+
+
+    }
+
+    void reshuffle(GameObject[] texts)
+    {
+        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < texts.Length; t++)
+        {
+            GameObject tmp = texts[t];
+            int r = UnityEngine.Random.Range(t, texts.Length);
+            texts[t] = texts[r];
+            texts[r] = tmp;
+        }
+    }
+
+    public void isWon()
+    {
+        bool isWon = true;
+        foreach (GameObject glass in Glasses)
+        {
+            Transform glassTransform = glass.transform;
+            
+            string sphereColor = "";
+            foreach (Transform field in glassTransform)
+            {
+                if (field.tag == "Field")
+                {
+                    String str = "Field";
+                    string lastSphere = null;
+                    int count = 0;
+                    for (int i = 1; i < countOfSphereNeed + 1; i++)
+                    {
+                        Transform t = glass.transform.Find(str + i);
+                        foreach (Transform child in t)
+                        {
+                            if (child.tag == "BlueSphere" ||
+                               child.tag == "GreenSphere" ||
+                               child.tag == "RedSphere")
+                            {
+                                if(lastSphere== null)
+                                {
+                                    lastSphere = child.tag;
+                                    count++;
+                                }
+                                else if (lastSphere == child.tag)
+                                {
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+
+                    if (count != countOfSphereNeed && count!=0)
+                    {
+                        isWon = false;
+                        return;
+                    }
+                }
+            }
+        }
+
+        if (isWon)
+        {
+            Debug.Log("basarılı");
+        }
+
+
+    }
+}
---
+++ Separate Colors/Assets/Scripts/Sphere.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class Sphere : MonoBehaviour
+{
+    public Vector3 screenPoint;
+    public Vector3 offset;
+    private bool isLookingFor = false;
+    public GameObject gameControl;
+    private GameObject goTo; // field objesi var
+    private bool isHold = false;
+    Sequence seq;
+
+    private void Start()
+    {
+    }
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    private void OnMouseUp()
+    {
+        if (isHold)
+        {
+            Vector3 curPosition = transform.position;
+            curPosition.z += .1f;
+            transform.position = curPosition;
+            Vector3 target;
+            if (!isLookingFor)
+            {
+                seq = DOTween.Sequence();
+                target = goTo.transform.position;
+                target.y = 2.5f;
+                seq.Append(transform.DOMove(target, .8f));
+                seq.Append(transform.DOMove(DeepestEmptyField(goTo).transform.position, 1f));
+                transform.parent = DeepestEmptyField(goTo).transform;
+
+                gameControl.GetComponent<GameController>().isWon();
+            }
+            else
+            {
+                target = transform.parent.gameObject.transform.position;
+                seq.Append(transform.DOMove(target, 0.8f));
+            }
+        }
+
+
+    }
+
+    private GameObject DeepestEmptyField(GameObject field)
+    {
+        GameObject glass = field.transform.parent.gameObject;
+        Transform f1 = glass.transform.Find("Field1");
+        if (isFilled(f1))
+        {
+            Transform f2 = glass.gameObject.transform.Find("Field2");
+            if (isFilled(f2))
+            {
+                Transform f3 = glass.gameObject.transform.Find("Field3");
+                if (isFilled(f3))
+                {
+                    return null;
+                }
+                else
+                {
+                    return f3.gameObject;
+                }
+            }
+            else
+            {
+                return f2.gameObject;
+            }
+        }
+        else
+        {
+            return f1.gameObject;
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        curPosition.z -= .1f;
+        //curPosition.y -= -.5f;
+
+        if (findTheFieldAbove() == null)
+        {
+            transform.position = curPosition;
+            isHold = true;
+        }
+    }
+
+    private GameObject findTheFieldAbove()
+    {
+        GameObject res = null;
+
+        GameObject glass = transform.parent.gameObject.transform.parent.gameObject;
+        int field = transform.parent.name[transform.parent.name.Length - 1] - '0';
+        field++;
+        string nextField = "Field" + field;
+//        Debug.Log(nextField);
+        Transform f = glass.transform.Find(nextField);
+
+        if (f != null)
+        {
+            if (isFilled(f))
+            {
+                foreach (Transform child in f)
+                {
+
+                    res = child.gameObject;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Field")
+        {
+            if (isMovable(other))
+            {
+                isLookingFor = false;
+                goTo = other.gameObject;
+            }
+
+        }
+        else if (other.transform.tag == "BlueSphere" ||
+              other.transform.tag == "GreenSphere" ||
+              other.transform.tag == "RedSphere")
+        {
+            goTo = other.transform.parent.gameObject;
+        }
+    }
+
+    private bool isMovable(Collider other)
+    {
+        GameObject glass = other.transform.parent.gameObject;
+        Transform f1 = glass.transform.Find("Field1");
+
+        if (isFilled(f1))
+        {
+            Transform f2 = glass.gameObject.transform.Find("Field2");
+            if (isFilled(f2))
+            {
+                Transform f3 = glass.gameObject.transform.Find("Field3");
+                if (isFilled(f3))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private bool isFilled(Transform t)
+    {
+
+
+        foreach (Transform child in t)
+        {
+            if (child.tag == "BlueSphere" ||
+               child.tag == "GreenSphere" ||
+               child.tag == "RedSphere")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.transform.tag == "Field")
+        {
+            isLookingFor = true;
+        }
+    }
+
+}
